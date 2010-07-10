552a4b47c96e2d2eb070f7b8e3c47b8584cac0c4
John Smith <onefloridacoder@live.com>
2010-07-10 10:49:20 -0400

    Adding HowdyCloudy project and Azure repo directory; Adding RIA service Kata directories and source.

---
+++ Logging/src/Logging.Components/Enums/ScreenKeyType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logging.Components
+{
+    public enum ScreenKeyType
+    {
+        Shell = 0,
+        Admin,
+        Login
+    }
+}
---
+++ Logging/src/Logging.Components/IsoStoreService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logging.Components
+{
+    using System.Linq;
+    using System.Collections.Generic;
+    using FMS.UI.Infrastructure;
+
+    public class IsoStoreService : IIsoStoreService
+    {
+        public IsoStoreFront FetchStoreFront()
+        {
+            return this.IsoStoreFront;
+        }
+
+        private IsoStoreFront isoStoreFront;
+        public IsoStoreFront IsoStoreFront
+        {
+            get
+            {
+                if (this.isoStoreFront == null)
+                {
+                    this.isoStoreFront = new IsoStoreFront();
+                }
+                return this.isoStoreFront;
+            }
+        }
+
+        public void WriteToStore(IList<TraceEntry> traceEntries)
+        {
+            traceEntries.ToList().ForEach(te => this.IsoStoreFront.Add(te.ScreenKeyType, te));
+        }
+
+        public void WriteToStore(IList<LogEntry> logEntries)
+        {
+            logEntries.ToList().ForEach(le => this.IsoStoreFront.Add(le.ScreenKeyType, le));
+        }
+    }
+
+}
