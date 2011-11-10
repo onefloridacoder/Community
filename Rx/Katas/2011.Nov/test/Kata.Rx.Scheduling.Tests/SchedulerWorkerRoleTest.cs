888ef7f51410449381f0f4e95a92ca6bff3f8474
onefloridacoder <onefloridacoder@live.com>
2011-11-10 16:16:07 -0500

    New Rx Kata

---
+++ Rx/Katas/2011.Nov/test/Kata.Rx.Scheduling.Tests/SchedulerWorkerRoleTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata.Rx.Scheduling.Tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class SchedulerWorkerRoleTest
+    {
+        public SchedulerWorkerRoleTest() { /* TODO: Add constructor logic here  - NOT!!! */ }
+
+        [TestMethod]
+        public void CanPutItemsOnTheQueue()
+        {
+            // Create/Find a queue
+
+            // Create a reference to the queue
+
+            // Create state objects and add them to the queue
+
+            // Assert.IsTrue(QueueReference.ItemsAreOnQueue)
+        }
+
+        [TestMethod]
+        public void CanPeekAtItemsOnTheQueue()
+        {
+            // Create/Find a queue
+
+            // Create a reference to the queue
+
+            // Create state objects and add them to the queue
+
+            // Assert.IsTrue(QueueReference.PeekAt(firstItemOnQueue) != null);
+        }
+
+        [TestMethod]
+        public void CanPopItemsOffTheQueue()
+        {
+            // Create/Find a queue
+
+            // Create a reference to the queue
+
+            // Create state objects and add them to the queue
+
+            // Assert.IsTrue(QueueReference.ItemsAreOnQueue)
+        }
+    }
+}
