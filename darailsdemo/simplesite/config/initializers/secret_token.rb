df4419c890e06c0fe18d0fe87ac8426fc76fdf7e
John Smith <onefloridacoder@live.com>
2011-11-24 18:07:43 -0500

    Initial Commit

---
+++ darailsdemo/simplesite/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplesite::Application.config.secret_token = '83839da03a40330532c3c23fc87971dd08df8a8e0cc342d8c6b7159ea826d26013cb5002821c3b04a13a41713e2e72d073fdd78d8a8c6cf1ab13ebf151050faf'
